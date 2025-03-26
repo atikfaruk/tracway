0750ac358d6f7330d531f1276fdf4c95ac5ebf91
atikfaruk <aatikfaruk@gmail.com>
2025-03-26 17:57:37 +0600

    package page background added

--- app/packages/page.jsx
+++ app/packages/page.jsx
@@ -5,7 +5,10 @@ const Packages = () => {
   return (
     <main className="main">
       {/* heading */}
-      <PageHeading bg="/pages/about-bg.jpg" title="Tour packages" />
+      <PageHeading
+        bg="https://res.cloudinary.com/dazc5yfhr/image/upload/v1742990223/giau-tran-ZMdXvd6jGww-unsplash_ezoi1z.jpg"
+        title="Tour packages"
+      />
 
       {/* under construction */}
       <UnderConstruction />
